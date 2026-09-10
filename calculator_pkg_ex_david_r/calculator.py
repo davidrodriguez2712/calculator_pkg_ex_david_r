55a3387be63a41661a0a895ee4a0b29e8cb93f53
David Rodriguez <rodriguezdavid2712@gmail.com>
2026-09-09 20:28:41 -0500

    ready to deploy

--- calculator_pkg_ex_david_r/calculator.py
+++ calculator_pkg_ex_david_r/calculator.py
@@ -1,4 +1,7 @@
 class Calculator:
+    def __init__(self):
+        print("HELLO")
+
     def add(self, num1, num2):
         return num1 + num2
     
---
+++ scripts/run.py
@@ -0,0 +1,3 @@
+from calculator_pkg_ex_david_r import Calculator
+
+print(Calculator().add(1, 2))
