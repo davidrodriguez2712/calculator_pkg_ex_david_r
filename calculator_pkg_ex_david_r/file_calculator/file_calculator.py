c5d527ba4778b58c6ffd13ec6cbde94ad135af22
David Rodriguez <rodriguezdavid2712@gmail.com>
2026-09-10 16:08:06 -0500

    update

---
+++ calculator_pkg_ex_david_r/file_calculator/file_calculator.py
@@ -0,0 +1,8 @@
+from ..calculator import Calculator # relative import
+# from calculator_pkg_ex_david_r.calculator import Calculator # abolute import 
+
+
+class FileCalculator(Calculator):
+    def __init__(self):
+        pass
+
