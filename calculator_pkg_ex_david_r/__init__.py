f7ae580b85f014842510672ec8d94e27d384410a
David Rodriguez <rodriguezdavid2712@gmail.com>
2026-09-10 16:39:30 -0500

    file calculator implemented

--- calculator_pkg_ex_david_r/__init__.py
+++ calculator_pkg_ex_david_r/__init__.py
@@ -1,3 +1,5 @@
 from .calculator import Calculator
+from .file_calculator import FileCalculator
+#from . import file_calculator # es el folder o carpeta del paquete completo
 
 __all__ = ["Calculator"]
--- calculator_pkg_ex_david_r/file_calculator/file_calculator.py
+++ calculator_pkg_ex_david_r/file_calculator/file_calculator.py
@@ -1,8 +1,22 @@
 from ..calculator import Calculator # relative import
 # from calculator_pkg_ex_david_r.calculator import Calculator # abolute import 
-
+from pathlib import Path
 
 class FileCalculator(Calculator):
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        path= Path(__file__).parent / "nums.csv"
+    ):
+        self.path = path
+
+    def add_file(self):
+        total = None
+        with open(self.path) as f:
+            for line in f:
+                if total is None:
+                    total = line
+                    continue
+                else:
+                    total += line
+        return total
 
